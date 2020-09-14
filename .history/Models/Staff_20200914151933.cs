7fa154ebb815984adb07d7a393618934d4be25e4
Ngozi Izuchi G <49325449+giprim@users.noreply.github.com>
2020-09-14 15:22:36 +0100

    feat: setup Staff

---
+++ .history/Models/Staff_20200914151933.cs
@@ -0,0 +1,9 @@
+namespace inventory.Models
+{
+    public class Staff
+    {
+        public int StaffId {get; set;}
+        public string FirstName {get; set;}
+        public string LastName {get;set;}
+    }
+}
